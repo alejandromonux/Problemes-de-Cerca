504e21aa69cda0be8f9a34dab32d0966bd2ca57f
Neil <neil.torrero@students.salle.url.edu>
2020-05-22 13:27:02 +0200

    fix: Sorts and appends correctly

--- Algorismes/CSP/CSP.py
+++ Algorismes/CSP/CSP.py
@@ -1,8 +1,8 @@
-#Marc: Notes
-#ALEX: EL CAMINO MÁS CORTO NO ES EL QUE TIENE MENOS ELEMENTOS SI NO EL QUE TIENE MENOR COSTE TOTAL???
-#-------------------------------------------------------------------------------------------------------------------------------------------
-#h'1 = escollir variable més restringida -> La que té menys cost
-#------------------------------------------------------------------------------------------------------------------------------------------
+# Marc: Notes
+# ALEX: EL CAMINO MÁS CORTO NO ES EL QUE TIENE MENOS ELEMENTOS SI NO EL QUE TIENE MENOR COSTE TOTAL???
+# -------------------------------------------------------------------------------------------------------------------------------------------
+# h'1 = escollir variable més restringida -> La que té menys cost
+# ------------------------------------------------------------------------------------------------------------------------------------------
 
 """
 1. Estat inicial: cap variable té un valor assignat.
@@ -12,17 +12,20 @@
 5. Es torna al pas 2 si cal.
 """
 
+
 def getCostTotal(n):
     return n.costAcumulat
 
+
 def getCostCami(n):
     return len(n.desti.camins)
+
 
 def CSP(nodeOrigen, nodeDesti):
     acabar = 0
     n = nodeOrigen
     while acabar == 0:
-        #Busquem si podem arribar ja al destí desde aqui
+        # Busquem si podem arribar ja al destí desde aqui
         for cami in n.camins:
             if nodeDesti.nom == cami.desti.nom:
                 n.trace.append(cami.desti)
@@ -30,13 +33,15 @@ def CSP(nodeOrigen, nodeDesti):
                 cami.desti.costAcumulat = n.costAcumulat + cami.cost
                 return cami.desti
 
+        # caminsOrdenats = n.camins
+        # caminsOrdenats.sort(key = getCostCami)
+        # següentCiutat = caminsOrdenats.pop(0)
 
-        #caminsOrdenats = n.camins
-        #caminsOrdenats.sort(key = getCostCami)
-        #següentCiutat = caminsOrdenats.pop(0)
-        
-        #Agafem la següent ciutat amb cost menor i avançem per allà
-        següentCiutat = n.camins.sort(key = getCostCami).pop(0)
+        # Agafem la següent ciutat amb cost menor i avançem per allà
+        caminsOrdenats = n.camins
+
+        caminsOrdenats.sort(key=getCostCami, reverse=True)
+        següentCiutat = caminsOrdenats.pop()
         n.trace.append(següentCiutat.desti)
         següentCiutat.desti.trace = n.trace
         següentCiutat.desti.costAcumulat = n.costAcumulat + següentCiutat.cost
